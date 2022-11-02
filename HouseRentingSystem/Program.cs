bbc6805bc144e8c4aff729c4c3e1d7b2d4b067ba
Simeon Raychev <simeon0022@gmail.com>
2022-11-02 23:08:55 +0200

    Adding favicon

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,4 +1,5 @@
-using HouseRentingSystem.Data;
+using HouseRentingSystem.Infrastructure.Data;
+using HouseRentingSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
